4245e401e53297aa0414eae6fec895aa8e2ede9c
Rachna Ralhan <info@rachnaradio.com>
2018-04-27 11:12:59 -0700

    Adding Project Files

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Rachna',
+	username: 'ralhan',
+	email: 'racral@gmail.com'
+},
+{
+	id: 2,
+	name: 'Yogi',
+	username: 'yahuja',
+	email: 'yahuja@gmail.com'
+},
+{
+	id: 3,
+	name: 'Alisha',
+	username: 'ali',
+	email: 'ali@gmail.com'
+},
+{
+	id: 4,
+	name: 'Neil',
+	username: 'neila',
+	email: 'neil@gmail.com'
+},
+{
+	id: 5,
+	name: 'Rhea',
+	username: 'rheaa',
+	email: 'rhea@gmail.com'
+}
+]
